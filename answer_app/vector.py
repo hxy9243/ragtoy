fa986205741bdf36e35b09449d4c0c09d5b332b3
Kevin Hu <hxy9243@gmail.com>
2023-03-21 06:00:25 +0000

    create basic vectorsearch module

---
+++ answer_app/vector.py
@@ -0,0 +1,19 @@
+
+class VectorSearch:
+
+    def __init__(self):
+
+        # init connections
+        pass
+
+    def create(self, key):
+
+        pass
+
+    def put(self, key, vector, metadata):
+
+        pass
+
+    def search(self, key, vecotr, max=5):
+
+        pass
