2c6e7d7f2af5d938ad9fed43bfb894d90aefc332
Kevin Hu <hxy9243@gmail.com>
2023-04-01 21:56:41 +0000

    reorg redis impleementation

--- answer_app/vector.py
+++ answer_app/vector.py
@@ -1,5 +1,7 @@
 import redis
-from redis.commands.search.field import VectorField, TagField
+from redis.commands.search.field import (
+    VectorField, TagField, TextField
+)
 from redis.commands.search.query import Query
 
 import numpy as np
@@ -7,46 +9,69 @@ import numpy as np
 GPT3_EMBEDDING_SIZE = 2048
 
 
-class VectorSearch:
-
+class VectorIndices:
     def __init__(self,
-                 index,
                  host='localhost',
-                 port=6379,
-                 embedding_size=GPT3_EMBEDDING_SIZE,
-                 ):
+                 port=6379):
         self.conn = redis.Redis(host=host, port=port)
-        self.index = index
-        self.embedding_size = embedding_size
 
-    def create_index(self):
+    def list(self):
+        return self.conn.execute_command('ft._list')
+
+    def get(self, idx):
+        return VectorIndex(self.conn, idx)
+
+    def create(self, idx, embedding_size=GPT3_EMBEDDING_SIZE):
+        vi = VectorIndex(self.conn, idx, embedding_size)
+        vi.create_index(embedding_size)
+
+        return vi
+
+
+class VectorIndex:
+    def __init__(self,
+                 conn,
+                 index,
+                 ):
+        self.conn = conn
+        self.index = index
+
+    def create_index(self, embedding_size=GPT3_EMBEDDING_SIZE):
         schema = [
             VectorField('embedding',
                         'hnsw',
                         {
                             "TYPE": 'FLOAT32',
-                            "DIM": self.embedding_size,
+                            "DIM": embedding_size,
                             "DISTANCE_METRIC": 'cosine',
                         }),
-            TagField('text_id'),
+            TagField('tag'),
+            TextField('text'),
         ]
         self.conn.ft(self.index).create_index(schema)
+
+    def info(self):
+        self.conn.ft(self.index).info()
 
     def drop_index(self):
         self.conn.ft(self.index).dropindex(delete_documents=True)
 
-    def put(self, key, embedding, text_id):
+    def put(self, key, tag, text, embedding):
         self.conn.hset(key, mapping={
             'embedding': embedding.tobytes(),
-            'text_id': text_id,
+            'tag': tag,
+            'text': text,
         })
         pass
 
     def get(self, key, field):
         return self.conn.hget(key, field)
 
-    def get_textid(self, key):
-        return self.get(key, 'text_id').decode('utf-8')
+    def get_tag(self, key):
+        return self.get(key, 'tag').decode('utf-8')
+
+    def get_text(self, key):
+        return self.get(key, 'text').decode('utf-8')
 
     def get_embedding(self, key):
         data = self.get(key, 'embedding')
@@ -55,7 +80,7 @@ class VectorSearch:
     def search(self, vector, max=5):
         querystr = f'*=>[KNN {max} @embedding $vector AS vector_score]'
         query = Query(querystr).return_fields(
-            'vector_score', 'embedding', 'text_id',
+            'vector_score', 'embedding', 'tag', 'text',
         ).sort_by('vector_score').dialect(2)
 
         results = self.conn.ft(self.index).search(
@@ -69,6 +94,7 @@ class VectorSearch:
             ret.append({
                 'key': r.id,
                 'vector_score': r.vector_score,
-                'text_id': r.text_id,
+                'tag': r.tag,
+                'text': r.text,
             })
         return ret
--- answer_app/vector_test.py
+++ answer_app/vector_test.py
@@ -2,17 +2,16 @@ import pytest
 
 import numpy as np
 
+from vector import VectorIndices
 
-from vector import VectorSearch
-
-EMBEDDING_SIZE = 8
+TEST_EMBEDDING_SIZE = 8
 
 
 def _create_random_vectors():
     vectors = []
     for i in range(10):
         vectors.append(
-            np.random.random(EMBEDDING_SIZE).astype(np.float32)
+            np.random.random(TEST_EMBEDDING_SIZE).astype(np.float32)
         )
     return vectors
 
@@ -22,43 +21,45 @@ def cosine(v1, v2):
 
 
 @pytest.fixture
-def vector_search():
-    return VectorSearch('testidx', embedding_size=EMBEDDING_SIZE)
+def vector_index():
+    indices = VectorIndices()
+
+    return indices.get('testidx')
 
 
-def test_initvector(vector_search):
-    vector_search.create_index()
+def test_initindex(vector_index):
+    vector_index.create_index(embedding_size=TEST_EMBEDDING_SIZE)
 
 
-def test_putvector(vector_search):
+def test_putvector(vector_index):
     vectors = _create_random_vectors()
 
     for i in range(10):
         data = vectors[i]
-        vector_search.put(i, data, 'text-'+str(i))
+        vector_index.put(i, 'text-'+str(i), 'example random text', data)
 
     # test get textid
-    textid = vector_search.get_textid(1)
+    textid = vector_index.get_tag(1)
     assert textid == 'text-1', \
         'ValueError: expecting text-1, getting ' + textid
 
     # test get embedding
     for i in range(10):
-        embedding = vector_search.get_embedding(i)
+        embedding = vector_index.get_embedding(i)
 
         assert np.array_equal(embedding, vectors[i]), \
             f'Error, getting unexpected embedding value at index {i}'
 
 
-def test_searchvector(vector_search):
-    search_vector = np.random.random(EMBEDDING_SIZE).astype(np.float32)
+def test_searchvector(vector_index):
+    search_vector = np.random.random(TEST_EMBEDDING_SIZE).astype(np.float32)
 
-    results = vector_search.search(search_vector, max=10)
+    results = vector_index.search(search_vector, max=10)
     scores = []
 
     for r in results:
         print(r)
-        embedding = vector_search.get_embedding(r['key'])
+        embedding = vector_index.get_embedding(r['key'])
         score = cosine(search_vector, embedding)
         scores.append(score)
 
@@ -67,6 +68,5 @@ def test_searchvector(vector_search):
     assert scores == sorted(scores), 'Search result not sorted'
 
 
-def test_cleanup(vector_search):
-    vector_search.drop_index()
-
+def test_cleanup(vector_index):
+    vector_index.drop_index()
