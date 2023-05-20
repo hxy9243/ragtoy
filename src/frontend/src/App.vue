f6deb8eb02215624b64cff37a50ac25e927ea975
Kevin Hu <hxy9243@gmail.com>
2023-05-20 17:33:49 +0000

    create an example small frontend

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <DocumentList url="http://localhost:5000" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import DocumentList from './components/DocumentList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    DocumentList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/frontend/src/components/DocumentList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="document-list">
+        <h1>Document List</h1>
+        <ul>
+            <li v-for="document in documents" :key="document.id">{{ document.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['url'],
+    data() {
+        return {
+            documents: [],
+        };
+    },
+    mounted() {
+        this.fetchDocuments();
+    },
+    methods: {
+        fetchDocuments() {
+            // Make an API request to fetch the documents from your backend
+            // Replace the URL with your actual API endpoint
+            fetch(this.url + '/api/documents')
+                .then((response) => response.json())
+                .then((data) => {
+                    this.documents = data;
+                })
+                .catch((error) => {
+                    console.error('Error fetching documents:', error);
+                });
+        },
+    },
+};
+</script>
