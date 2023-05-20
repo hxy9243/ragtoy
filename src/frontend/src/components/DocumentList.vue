4368d056ff4efacbb486c58bef70b1357afd0005
Kevin Hu <hxy9243@gmail.com>
2023-05-20 23:33:39 +0000

    create basic document managing app

--- src/frontend/src/components/DocumentList.vue
+++ src/frontend/src/components/DocumentList.vue
@@ -1,10 +1,78 @@
 <template>
     <div class="document-list">
         <h1>Document List</h1>
-        <ul>
-            <li v-for="document in documents" :key="document.id">{{ document.name }}</li>
-        </ul>
     </div>
+
+    <v-row align="center" justify="center">
+        <v-col cols=8 md=6>
+            <v-expansion-panels>
+                <v-expansion-panel title="Documents" variant="accordion">
+                    <v-expansion-panel-text>
+                        <v-container>
+                            <v-row>
+                                <v-col cols=4>
+                                    <v-card>
+                                        <v-card-item>
+                                            <v-row>
+                                                <v-select v-model=selectedDocument label="Documents" :items=this.documents
+                                                    item-title="name" item-value="id"
+                                                    @update:modelValue="handleSelectDocument"></v-select>
+                                            </v-row>
+                                        </v-card-item>
+
+                                        <v-card-actions>
+                                            <v-row>
+                                                <v-btn color="red">Delete</v-btn>
+
+                                                <v-btn color="green">Add
+                                                    <v-dialog v-model="dialog" activator="parent">
+                                                        <v-card>
+                                                            <v-card-text>
+                                                                <v-row>
+                                                                    Create a new Document
+                                                                </v-row>
+                                                            </v-card-text>
+                                                            <v-card-item>
+                                                                <v-row>
+                                                                    <v-file-input label="File input"
+                                                                        variant="solo-filled"></v-file-input>
+                                                                </v-row>
+                                                            </v-card-item>
+                                                            <v-card-actions>
+                                                                <v-row>
+                                                                    <v-col>
+                                                                        <v-btn color="red" block
+                                                                            @click="dialog = false">Close
+                                                                            Dialog</v-btn>
+                                                                    </v-col>
+                                                                    <v-col>
+                                                                        <v-btn color="green" block
+                                                                            @click="dialog = false">Add</v-btn>
+                                                                    </v-col>
+                                                                </v-row>
+                                                            </v-card-actions>
+                                                        </v-card>
+
+                                                    </v-dialog>
+                                                </v-btn>
+                                            </v-row>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-col>
+                                <v-col cols=8>
+                                    <div>
+                                        <v-textarea v-model="documentBody" readonly=false>
+                                        </v-textarea>
+                                    </div>
+                                </v-col>
+
+                            </v-row>
+                        </v-container>
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+            </v-expansion-panels>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -13,6 +81,10 @@ export default {
     data() {
         return {
             documents: [],
+            documentMap: {},
+            documentBody: '',
+            selectedDocument: null,
+            dialog: null,
         };
     },
     mounted() {
@@ -26,10 +98,19 @@ export default {
                 .then((response) => response.json())
                 .then((data) => {
                     this.documents = data;
+                    this.documentMap = data.reduce((map, object) => {
+                        map[object.id] = object;
+                        return map;
+                    }, {});
                 })
                 .catch((error) => {
                     console.error('Error fetching documents:', error);
                 });
+        },
+        handleSelectDocument(value) {
+            console.log(this.documentMap[value].body);
+
+            this.documentBody = this.documentMap[value].body;
         },
     },
 };
