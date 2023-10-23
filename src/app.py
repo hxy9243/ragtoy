56a535889b208feeb2e0437fddaed7efb97583f8
Kevin Hu <hxy9243@gmail.com>
2023-10-23 06:02:02 +0000

    add run server options

--- src/app.py
+++ src/app.py
@@ -10,14 +10,28 @@ def main():
 
     subparsers = argparser.add_subparsers(dest='command')
     subparsers.add_parser('init')
-    subparsers.add_parser('run')
+    run_parser = subparsers.add_parser('run')
+
+    run_parser.add_argument('--host', action='store',
+                            default='127.0.0.1', dest='host',
+                            help='The host IP for server to bind to')
+    run_parser.add_argument('-p,--port', action='store',
+                            default=5000, dest='port',
+                            help='The port for server to listen on')
+    run_parser.add_argument('-d,--debug', action='store_true',
+                            dest='debug',
+                            help='Turns on debug')
 
     args = argparser.parse_args()
 
     if args.command == 'init':
         initapp()
     elif args.command == 'run':
-        runapp(debug=True)
+        runapp(
+            host=args.host,
+            port=args.port,
+            debug=args.debug,
+        )
     else:
         argparser.print_help()
         logging.error('No command specified')
