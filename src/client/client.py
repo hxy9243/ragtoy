4e670a50f570b8cc214954a99e9597207ff01d1c
Kevin Hu <hxy9243@gmail.com>
2023-11-04 21:48:04 +0000

    add a quick client impl

---
+++ src/client/client.py
@@ -0,0 +1,46 @@
+
+class Client:
+
+    def __init__(self):
+
+        pass
+
+    def add_document(self, doc):
+
+        pass
+
+    def get_documents(self):
+
+        pass
+
+    def get_document(self, docid):
+
+        pass
+
+    def delete_documents(self, docid):
+
+        pass
+
+    def add_conversation(self, docid, conv):
+
+        pass
+
+    def get_conversations(self, docid):
+
+        pass
+
+    def get_conversation(self, convid):
+
+        pass
+
+    def delete_conversation(self, convid):
+
+        pass
+
+    def add_message(self, convid, msg):
+
+        pass
+
+    def get_messages(self, convid):
+
+        pass
