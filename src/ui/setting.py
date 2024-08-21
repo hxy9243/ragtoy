4135766522b6c765cb76a7ac26570fd6b69ecc69
Kevin Hu <hxy9243@gmail.com>
2024-08-21 00:58:03 -0700

    remove old ui src

--- src/ui/setting.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-class SettingComponent:
-
-    def render(self):
-        gr.Textbox(label='OpenAI Token',
-                    placeholder='st-xxxx',
-                    type='password')
