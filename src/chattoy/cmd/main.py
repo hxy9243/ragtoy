419e860e2c108dfc92e0f074f2435fbaa236242c
Kevin Hu <hxy9243@gmail.com>
2024-08-24 16:34:12 -0700

    add new cmd interface impl

--- src/chattoy/cmd/main.py
+++ src/chattoy/cmd/main.py
@@ -1,33 +1,68 @@
 from typing import List
 
-class Command:
-    def __init__(self) -> None:
-        doc = Documents()
+import sys
+import logging
 
-    def ask(self, prompt: str):
-        pass
+import click
 
-
-class Documents:
-    def __init__(self, cfg) -> None:
-        cfg = cfg
-
-    def add(self, path: str):
-        pass
-
-    def ls(self) -> List[str]:
-        pass
-
-    def rm(self, id: str):
-        pass
-
-    def search(self, prompt: str) -> List[str]:
-        pass
-
-    def related(self, docid: str, limit=5) -> List[str]:
-        pass
+logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler(sys.stdout)])
 
 
-def main():
+@click.command(help="Ask questions based on all the documents in the DB")
+@click.argument("prompt")
+def ask(prompt: str):
+    logging.debug(f"Ask a question: {prompt}")
 
+
+@click.group(help="Document related operations")
+def document():
     pass
+
+
+@click.command(help="Add a new document or a directory defined by path")
+@click.argument("path")
+def add(path: str):
+    logging.debug(f"Adding a new path {path}")
+
+
+@click.command(help="List the documents")
+@click.argument("docid", default=None)
+def ls(docid: str | None):
+    logging.debug(f"Listing documents")
+
+
+@click.command(help="Remove a document")
+@click.argument("docid")
+def rm(docid: str):
+    logging.debug(f"Remove a document {docid}")
+
+
+@click.command(help="Search the most related documents based on prompt")
+@click.argument("prompt")
+def search(prompt: str):
+    logging.debug(f"Search a document based on prompt: {prompt}")
+
+
+@click.command(help="")
+@click.argument("docid")
+def related(docid: str):
+    logging.debug(f"find the most related documents with: {docid}")
+
+
+document.add_command(add)
+document.add_command(ls)
+document.add_command(rm)
+document.add_command(search)
+document.add_command(related)
+
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(document)
+main.add_command(ask)
+
+if __name__ == "__main__":
+    main()
